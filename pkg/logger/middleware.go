aab4396e9b23e42090b1d1da3d462124695a794f
王一之 <yz@ggnb.top>
2023-05-18 14:04:58 +0800

    feat: 日志中间件添加开始时间

--- pkg/logger/middleware.go
+++ pkg/logger/middleware.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"context"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
@@ -13,11 +14,14 @@ const loggerKey loggerContextKeyType = iota
 
 func Middleware(logger *zap.Logger) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		tm := time.Now()
 		logger := logger.With(
 			zap.String("client_ip", ctx.ClientIP()),
 			zap.String("method", ctx.Request.Method),
 			zap.String("path", ctx.Request.URL.Path),
 			zap.String("user_agent", ctx.Request.UserAgent()),
+			// 请求开始时间
+			zap.Time("start_time", tm),
 		)
 		ctx.Request = ctx.Request.WithContext(ContextWithLogger(ctx.Request.Context(), logger))
 		// 处理错误日志
