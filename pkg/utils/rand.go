08a8d5951c1f4936cc51361ea871f31d109f2c40
王一之 <yz@ggnb.top>
2023-05-30 00:42:29 +0800

    fix: 修复随机数为0000的问题

--- pkg/utils/rand.go
+++ pkg/utils/rand.go
@@ -3,15 +3,12 @@ package utils
 import (
 	"math/rand"
 	"time"
-	"unsafe"
 )
 
 const letterBytes = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 const (
-	letterIdxBits = 6                    // 6 bits to represent a letter index
-	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
-	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+	letterIdxBits = 6 // 6 bits to represent a letter index
 )
 
 var src = rand.NewSource(time.Now().UnixNano())
@@ -27,18 +24,8 @@ const (
 func RandString(n int, randType RandStringType) string {
 	b := make([]byte, n)
 	l := int(10 + (randType * 26))
-	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
-	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
-		if remain == 0 {
-			cache, remain = src.Int63(), letterIdxMax
-		}
-		if idx := int(cache & letterIdxMask); idx < l {
-			b[i] = letterBytes[idx]
-			i--
-		}
-		cache >>= letterIdxBits
-		remain--
+	for i := 0; i < n; i++ {
+		b[i] = letterBytes[rand.Intn(l)]
 	}
-
-	return *(*string)(unsafe.Pointer(&b))
+	return string(b)
 }
